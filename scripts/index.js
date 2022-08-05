9564bf930d1b91a9b20143836e09b871d0084c08
Shalind0r <aleksandr.hnut@gmail.com>
2022-08-06 00:12:02 +0300

    Заменяет число деления из 3 на 5

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,23 @@
 'use strict';
 
-const xNumber = 3;
-const answer = prompt('Укажите любое кол-во целых чисел (через запятаю)');
+const xNumber = 5;
+const answer = prompt('Укажите любое кол-во целых чисел кратных 5 (через запятую)');
 
 if (answer === null) {
-    console.log('Ответ не получен!');
+   console.log('Ответ не получен!');
 } else {
-    const values = answer.split(',');
-    const result = [];
-    for (let i = 0; i < values.length; i++) {
-        const number = +values[i];
-        if (number !== 0 && number % xNumber === 0) {
-            result.push(number);
-        }
-    }
-    if (result.length > 0) {
-        console.log(`Найдены числа, кратные ${xNumber}, в количестве ${result.length}шт:`);
-        console.log(result.join(', '));
-    } else {
-        console.log(`Числа которые, кратны ${xNumber}, не найдены`)
-    }
+   const values = answer.split(',');
+   const result = [];
+   for (let i = 0; i < values.length; i++) {
+      const number = +values[i];
+      if (number !== 0 && number % xNumber === 0) {
+         result.push(number);
+      }
+   }
+   if (result.length > 0) {
+      console.log(`Найдены числа, кратные ${xNumber}, в количестве ${result.length}шт:`);
+      console.log(result.join(', '));
+   } else {
+      console.log(`Числа которые, кратны ${xNumber}, не найдены`)
+   }
 }
